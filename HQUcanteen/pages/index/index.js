36b90a19ffbbe1a308c6187c01befd51cf17d82c
hqulzq <1438281785@qq.com>
2021-09-02 22:23:34 +0800

    first commit

---
+++ HQUcanteen/pages/index/index.js
@@ -0,0 +1,128 @@
+// pages/pCenter/pCenter.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    quantityOfHeat : 1234,
+    menuList:[
+      {
+        id:0,
+        name:"铁板烧粤式和牛",
+        img:"/img/1 (1).jpg",
+        cateen:""
+      },
+      {
+        id:1,
+        name:"水煮咕呱",
+        img:"/img/1 (2).jpg",
+        cateen:""
+      },
+      {
+        id:2,
+        name:"猪扇骨",
+        img:"/img/1 (3).jpg",
+        cateen:"华中厨房"
+      },
+      {
+        id:3,
+        name:"麻辣兔肉",
+        img:"/img/1 (4).jpg",
+        cateen:"华中餐厅"
+      },
+      {
+        id:4,
+        name:"甘肃凉面",
+        img:"/img/1 (5).jpg",
+        cateen:"拉面馆"
+      },
+      {
+        id:5,
+        name:"新疆炒米粉",
+        img:"/img/1 (6).jpg",
+        cateen:""
+      },
+      {
+        id:6,
+        name:"西苑真“虾”减脂套餐",
+        img:"/img/1 (7).jpg",
+        cateen:"西苑餐厅"
+      },
+      {
+        id:7,
+        name:"怀旧厨房烩三鲜",
+        img:"/img/1 (8).jpg",
+        cateen:"怀旧厨房"
+      },
+      {
+        id:8,
+        name:"麻辣龙虾尾",
+        img:"/img/1 (9).jpg",
+        cateen:"西北厨房"
+      },
+      {
+        id:9,
+        name:"油泼面",
+        img:"/img/1 (10).jpg",
+        cateen:"西苑二楼拉面馆"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
